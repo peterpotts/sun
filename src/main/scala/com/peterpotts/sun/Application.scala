eee640d2058ad9a4ceb68b6dbebf9ccd903255e8
Peter Potts <peter.potts@cointrust.com>
2016-05-08 20:09:39 -0700

    Add open and close.

--- src/main/scala/com/peterpotts/sun/Application.scala
+++ src/main/scala/com/peterpotts/sun/Application.scala
@@ -10,11 +10,36 @@ object Application extends LazyLogging {
   def main(args: Array[String]): Unit = {
     val location = Location(latitude = 37.563, longitude = -122.3255)
     val sun = new Sun(location)
-    //val dateTime = new DateTime(2016, 5, 5, 14, 0, 0)
     val dateTime = new DateTime()
-    val position = sun.position(dateTime)
-    println(position)
+    println("LEFT BLIND")
+    def leftBlindCloseAt(dateTime: DateTime) = LeftBlind.close(sun.position(dateTime))
+    loop(leftBlindCloseAt, dateTime, 10)
+    println("RIGHT BLIND")
+    def rightBlindCloseAt(dateTime: DateTime) = RightBlind.close(sun.position(dateTime))
+    loop(rightBlindCloseAt, dateTime, 10)
   }
+
+  private def loop(closeAt: DateTime => Boolean, dateTime: DateTime, changes: Int): Unit =
+    loop(closeAt, closeAt(dateTime), dateTime, changes)
+
+  private def loop(closeAt: DateTime => Boolean, closed: Boolean, dateTime: DateTime, changes: Int): Unit =
+    if (changes > 0) {
+
+      val nextDateTime = dateTime.plusMinutes(1)
+
+      val close = closeAt(nextDateTime)
+
+      if (close == closed)
+        loop(closeAt, closed, nextDateTime, changes)
+      else {
+        if (close)
+          println(s"$nextDateTime - Close")
+        else
+          println(s"$nextDateTime - Open")
+
+        loop(closeAt, close, nextDateTime, changes - 1)
+      }
+    }
 }
 
 case class Location(latitude: Double, longitude: Double)
@@ -58,20 +83,29 @@ case class Sun(location: Location) {
 }
 
 trait Blind {
+  val forwardAzimuth = 140.0
+
   def close(position: Position): Boolean
 }
 
 case object LeftBlind extends Blind {
+  val browUp = 95.0
+  val browForward = 80.0
+  val leftWallForward = 123.0
+  val leftWallToRightWindow = 102.0
+  val rightWallForward = 87.0
+  val leftWindowToRightWall = 104.0
+
   def close(position: Position) =
     position.elevation > 0.0 &&
-      position.elevation < toDegrees(atan(95.0 / 80.0)) &&
-      position.azimuth > 140.0 - 90.0 + toDegrees(atan(123.0 / 102.0)) &&
-      position.azimuth < 140.0 + 90.0 - toDegrees(atan(87.0 / 104.0))
+      position.elevation < toDegrees(atan(browUp / browForward)) &&
+      position.azimuth > forwardAzimuth - 90.0 + toDegrees(atan(leftWallForward / leftWallToRightWindow)) &&
+      position.azimuth < forwardAzimuth + 90.0 - toDegrees(atan(rightWallForward / leftWindowToRightWall))
 }
 
 case object RightBlind extends Blind {
   def close(position: Position) =
     position.elevation > 0.0 &&
-      position.azimuth > 140.0 - 90.0 &&
-      position.azimuth < 140.0 + 90.0
+      position.azimuth > forwardAzimuth - 90.0 &&
+      position.azimuth < forwardAzimuth + 90.0
 }
